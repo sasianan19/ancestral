4d5fd06dc6b99db16a9e41ea54ba66081b994946
sasianan19 <sasianan19@gmail.com>
2022-08-05 21:53:32 -0600

    Add models
    Add the following models: Continents, Countries, AnimalClass, Vertebrates, Invertebrates. Countries model has continent attribute with foreign key to Continents model. Vertebrates and Invertebrates models has classification attribute with foreign key to AnimalClass model + country attribute with foreign key to Countries model.

--- totemProject/totemApp/models.py
+++ totemProject/totemApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Continents(models.Model):
+    continent = models.CharField(max_length=50)
+
+class Countries(models.Model):
+    country = models.CharField(max_length=100)
+    continent = models.ForeignKey(Continents, on_delete=models.CASCADE)
+
+class AnimalClass(models.Model):
+    classification = models.CharField(max_length=100)
+
+class Vertebrates(models.Model):
+    classification = models.ForeignKey(AnimalClass, on_delete=models.CASCADE)
+    animal = models.CharField(max_length=70)
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    meaning = models.TextField(max_length=1000) 
+
+class Invertebrates(models.Model):
+    classification = models.ForeignKey(AnimalClass, on_delete=models.CASCADE)
+    animal = models.CharField(max_length=70)
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    meaning = models.TextField(max_length=1000) 
