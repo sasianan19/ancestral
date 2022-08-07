6b908427c3dcc9e8cda23e799f29e67aaf7cb6e4
sasianan19 <sasianan19@gmail.com>
2022-08-07 11:24:13 -0600

    Update views.py
    Add three separate view functions to add country, vertebrate, and invertebrate to database. They all render on the same 'db-forms.html' template page. (CRUD) Creates entry in table in database.

--- totemProject/totemApp/views.py
+++ totemProject/totemApp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from totemApp.models import *
+from totemApp.forms import * 
 
 # Create your views here.
+def addCountry(request):
+    if request.method == "POST":
+        form = CountriesForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CountriesForm()
+    return render(request, 'db-forms.html', {'CountriesForm': form})
+
+
+def addVertebrate(request):
+    if request.method == "POST":
+        form = VertebratesForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = VertebratesForm()
+    return render(request, 'db-forms.html', {'VertebratesForm': form})
+
+
+def addInvertebrate(request):
+    if request.method == "POST":
+        form = InvertebratesForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = InvertebratesForm()
+    return render(request, 'db-forms.html', {'InvertebratesForm': form})
