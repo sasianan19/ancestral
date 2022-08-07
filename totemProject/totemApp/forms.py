57a8c7c1a2135a497640336400153b6e199066b7
sasianan19 <sasianan19@gmail.com>
2022-08-07 14:47:44 -0600

    Update forms.py
    Add 'forms.BooleanField(widget=forms.HiddenInput, initial=True)' to each model form so each form can be rendered and validated on same template page; rather than having 3 separate pages and views for each. Also add comments.

--- totemProject/totemApp/forms.py
+++ totemProject/totemApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from totemApp.models import *
 
-# The following two forms are commented out because they were only used to add a SET number of
+# The following two forms are commented out because they were only used to add a set number of
 # continents (7) and classifications (11). 
 # class ContinentsForm(forms.ModelForm):
 #     class Meta:
@@ -16,20 +16,28 @@ from totemApp.models import *
 #         labels = {'classification': "Classification"} 
 
 
+# Credit to Rafiq Hilali for "forms.BooleanField(widget=forms.HiddenInput, initial=True)" in order to have multiple forms on one page
+#  @ https://openclassrooms.com/en/courses/7107341-intermediate-django/7264795-include-multiple-forms-on-a-page
 class CountriesForm(forms.ModelForm):
+    c_r_u_dCountries = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = Countries
         fields = ['country', 'continent']
         labels = {'country': "Country", 'continent': "Continent"}
 
 class VertebratesForm(forms.ModelForm):
+    c_r_u_dVertebrates = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = Vertebrates
         fields = ['classification', 'animal', 'country', 'meaning']
-        labels = {'classification': "Class", 'animal': "Animal", 'country': "Country", 'meaning': "Meaning"}
+        labels = {'classification': "Class", 'animal': "Animal", 'country': "Country", 'meaning': "Symbolic Meaning"}
 
 class InvertebratesForm(forms.ModelForm):
+    c_r_u_dInvertebrates = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    
     class Meta:
         model = Invertebrates
         fields = ['classification', 'animal', 'country', 'meaning']
-        labels = {'classification': "Class", 'animal': "Animal", 'country': "Country", 'meaning': "Meaning"}
+        labels = {'classification': "Class", 'animal': "Animal", 'country': "Country", 'meaning': "Symbolic Meaning"}
