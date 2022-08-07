cb52c0bcc18eabaf936db3c54ffba96c7e8cd51c
sasianan19 <sasianan19@gmail.com>
2022-08-07 14:53:54 -0600

    Update views.py
    Rather than 3 separate view functions to add to each database table, consolidated into 1 view function for (CRUD) Create functionality.

--- totemProject/totemApp/views.py
+++ totemProject/totemApp/views.py
@@ -1,33 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from totemApp.models import *
 from totemApp.forms import * 
 
 # Create your views here.
-def addCountry(request):
-    if request.method == "POST":
-        form = CountriesForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = CountriesForm()
-    return render(request, 'db-forms.html', {'CountriesForm': form})
 
+# "create_view" is for adding to database tables
+# Credit to Rafiq Hilali for this code @ https://openclassrooms.com/en/courses/7107341-intermediate-django/7264795-include-multiple-forms-on-a-page
+def create_view(request):
+    addCountry = CountriesForm()
+    addVertebrate = VertebratesForm()
+    addInvertebrate = InvertebratesForm()
+    if request.method == 'POST':
+        if 'c_r_u_dCountries' in request.POST:
+            addCountry = CountriesForm(request.POST)
+            if addCountry.is_valid():
+                addCountry.save()
+                return redirect('createForms')
+        if 'c_r_u_dVertebrates' in request.POST:
+            addVertebrate = VertebratesForm(request.POST)
+            if addVertebrate.is_valid():
+                addVertebrate.save()
+                return redirect('createForms')
+        if 'c_r_u_dInvertebrates' in request.POST:
+            addInvertebrate = InvertebratesForm(request.POST)
+            if addInvertebrate.is_valid():
+                addInvertebrate.save()
+                return redirect('createForms')
+    context = {
+        "addCountry": addCountry,
+        "addVertebrate": addVertebrate,
+        "addInvertebrate": addInvertebrate,
+    }
+    return render(request, 'create_view.html', context=context)
 
-def addVertebrate(request):
-    if request.method == "POST":
-        form = VertebratesForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = VertebratesForm()
-    return render(request, 'db-forms.html', {'VertebratesForm': form})
-
-
-def addInvertebrate(request):
-    if request.method == "POST":
-        form = InvertebratesForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = InvertebratesForm()
-    return render(request, 'db-forms.html', {'InvertebratesForm': form})
