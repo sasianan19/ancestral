0e2159fd2b209a5895cace350343e003004eef6f
sasianan19 <sasianan19@gmail.com>
2022-08-07 22:41:14 -0600

    Update views.py
    Edit view names and return redirect to respective list views upon creation of data to be added to database tables (createView).

--- totemProject/totemApp/views.py
+++ totemProject/totemApp/views.py
@@ -6,7 +6,7 @@ from totemApp.forms import *
 
 # "create_view" is for adding to database tables
 # Credit to Rafiq Hilali for this code @ https://openclassrooms.com/en/courses/7107341-intermediate-django/7264795-include-multiple-forms-on-a-page
-def create_view(request):
+def createView(request):
     addCountry = CountriesForm()
     addKeyword = KeywordsForm()
     addVertebrate = VertebratesForm()
@@ -16,22 +16,22 @@ def create_view(request):
             addCountry = CountriesForm(request.POST)
             if addCountry.is_valid():
                 addCountry.save()
-                return redirect('createForms')
+                return redirect('countryList')
         if 'c_r_u_dKeywords' in request.POST:
             addKeyword = KeywordsForm(request.POST)
             if addKeyword.is_valid():
                 addKeyword.save()
-                return redirect('createForms')
+                return redirect('keywordList')
         if 'c_r_u_dVertebrates' in request.POST:
             addVertebrate = VertebratesForm(request.POST)
             if addVertebrate.is_valid():
                 addVertebrate.save()
-                return redirect('createForms')
+                return redirect('vertebrateList')
         if 'c_r_u_dInvertebrates' in request.POST:
             addInvertebrate = InvertebratesForm(request.POST)
             if addInvertebrate.is_valid():
                 addInvertebrate.save()
-                return redirect('createForms')
+                return redirect('invertebrateList')
     context = {
         "addCountry": addCountry,
         "addKeyword": addKeyword,
@@ -42,22 +42,32 @@ def create_view(request):
 
 
 
-def country_list_view(request):
+# Countries list, detail, update, & delete views
+def countryListView(request):
     countries = Countries.objects.all()
-   
     return render(request, 'country_list_view.html', {'countries': countries})
 
-def vertebrate_list_view(request):
+# def countryDetailView(request, id):
+
+
+
+
+# Keywords list, detail, update, & delete views
+def keywordListView(request):
+    keywords = Keywords.objects.all()
+    return render(request, 'keyword_list_view.html', {'keywords': keywords})
+
+
+
+# Vertebrates list, detail, update, & delete views
+def vertebrateListView(request):
     vertebrates = Vertebrates.objects.all()
-   
     return render(request, 'vertebrate_list_view.html', {'vertebrates': vertebrates})
 
-def invertebrate_list_view(request):
+
+
+# Invertebrates list, detail, update, & delete views
+def invertebrateListView(request):
     invertebrates = Invertebrates.objects.all()
-   
     return render(request, 'invertebrate_list_view.html', {'invertebrates': invertebrates})
 
-def keyword_list_view(request):
-    keywords = Keywords.objects.all()
-   
-    return render(request, 'keyword_list_view.html', {'keywords': keywords})
