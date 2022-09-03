719db3f499add3f4405dac062475b94c3e3afe0b
sasianan19 <sasianan19@gmail.com>
2022-09-03 17:07:21 -0600

    Add urls.py file to totemApp directory
    Cut urls from totemProject urls.py and paste paths into totemApp urls.py file.

---
+++ totemProject/totemApp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from totemApp.views import *
+
+urlpatterns = [
+    path('create', createEntry),
+
+    path('countries', countryIndex, name='countryIndex'),
+    path('updateCountry/<country_id>', updateCountry),
+    path('deleteCountry/<country_id>', deleteCountry),
+
+    path('keywords', keywordIndex, name='keywordIndex'),
+    path('updateKeyword/<keyword_id>', updateKeyword),
+    path('deleteKeyword/<keyword_id>', deleteKeyword),
+
+    path('vertebrates', vertebrateIndex, name='vertebrateIndex'),
+    path('updateVertebrate/<vertebrate_id>', updateVertebrate),
+    path('deleteVertebrate/<vertebrate_id>', deleteVertebrate),
+
+    path('invertebrates', invertebrateIndex, name='invertebrateIndex'),
+    path('updateInvertebrate/<invertebrate_id>', updateInvertebrate),
+    path('deleteInvertebrate/<invertebrate_id>', deleteInvertebrate),
+]
